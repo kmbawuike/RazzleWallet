b594026f6e3abf6f82b39d7bc1767f79522f2fda
EFLYTE-TECH <fridayemmanuel273@gmail.com>
2019-09-18 13:15:13 +0000

    i add the Experience file and also i have worked on the css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React, {Component, Fragment} from 'react'
 import '../../styles/default.css'
 import Footer from './Footer';
+import Experience from './Experience'
 
 class Home extends Component{
   render(){
     return(
       <Fragment>
-        <Footer/>
+        <Experience />
+        <Footer />
       </Fragment>
     )
   }
