a5789548d9116b98990f9903987f63582091b195
Emmanuel <godsonlive91@gmail.com>
2019-09-18 11:15:03 +0100

    working the footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React, {Component, Fragment} from 'react'
 import '../../styles/default.css'
+import Footer from './Footer';
 
 class Home extends Component{
   render(){
     return(
       <Fragment>
+        <Footer/>
       </Fragment>
     )
   }
