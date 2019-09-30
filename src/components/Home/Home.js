5f45d836a6d317b123a112cf39171e9f3b24371e
hola <henrijay00@gmail.com>
2019-09-30 12:47:23 +0100

    added the setup section of the body

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,13 @@ import React, {Component, Fragment} from 'react'
 import '../../styles/default.css'
 import Footer from './Footer';
 import Experience from './Experience'
+import BodySetup from './BodySetup';
 
 class Home extends Component{
   render(){
     return(
       <Fragment>
+        <BodySetup/>
         <Experience />
         <Footer />
       </Fragment>
