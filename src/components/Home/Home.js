d97d85ad16517f359411e3d69c6abced8b238518
sixtus <sixtusiwuchukwu21@gmail.com>
2019-09-30 13:25:31 +0100

    done styling the decription

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,15 @@ import '../../styles/default.css'
 import Footer from './Footer';
 import Experience from './Experience'
 import BodySetup from './BodySetup';
+import Description from './describtion'
 
 class Home extends Component{
   render(){
     return(
       <Fragment>
+        <Description />
         <BodySetup/>
+        
         <Experience />
         <Footer />
       </Fragment>
