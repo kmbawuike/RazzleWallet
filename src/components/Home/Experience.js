176ad4ebe81569f0c747e347feba66e9cb22db8b
EFLYTE-TECH <fridayemmanuel273@gmail.com>
2019-10-02 12:11:59 +0000

    i changed the responsiveness of the experience body and aslo added the playstore icons

--- src/components/Home/Experience.js
+++ src/components/Home/Experience.js
@@ -6,21 +6,22 @@ import Image2 from "../../assets/Home/play-store-button.png";
 class Experience extends React.Component {
   render() {
     return (
-      <div>
+      <div className="app">
         <center>
           <h1> Experience true freedom. </h1>
           <p>
-            You won 't be joining bad company. lots of people use Wallets
-            <pre> </pre>
-            everyday to get transaction done.
+            You won 't be joining bad company. lots of people use Wallets <pre/>
+             everyday to get transaction done.
+             
           </p>
         </center>
         <div className="pic">
-          <img src={Image1}/> 
-          <img src={Image2}/> 
+          <img src={Image1} id="mm" alt=""/> 
+          <img src={Image2} alt=""/> 
         </div>
       </div>
     );
   }
 }
 export default Experience;
+
